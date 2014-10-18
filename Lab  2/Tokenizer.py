0a4bb2bbe99f5b881128c886a5eb1f715ffd60a7
n8sk8gr8 <portville99@gmail.com>
2014-10-17 23:45:58 -0400

    Full Tokenizer

--- Lab  2/Tokenizer.py
+++ Lab  2/Tokenizer.py
@@ -12,6 +12,22 @@ class Tokenizer:
                 equals = equal.match(tokens) 
                 semicolon = semi.match(tokens)
                 cond_or = cond.match(tokens)
+                comma = comm.match(tokens)
+                explanation_mark = expl.match(tokens)
+                left_bracket = left_brack.match(tokens)
+                right_bracket = right_brack.match(tokens)
+                and_percent = and_per.match(tokens)
+                left_paren = left_par.match(tokens)
+                right_paren = right_par.match(tokens)
+                plus = add.match(tokens)
+                minus = sub.match(tokens)
+                times = mult.match(tokens)
+                not_equal = not_eq.match(tokens)
+                less_than = less.match(tokens)
+                greater_than = greater.match(tokens)
+                less_than_equal = less_than_eq.match(tokens)
+                greater_than_equal = greater_than_eq.match(tokens) 
+                
                 
                 if lower:
                         check_legal_token(tokens, lower)
@@ -42,7 +58,81 @@ class Tokenizer:
                         check_legal_token(tokens, cond_or)
                         decode_token(cond_or.group())
                         check_multi_token(tokens, cond_or)
-               
+                                          
+                elif comma:
+                        check_legal_token(tokens, comma)
+                        decode_token(comma.group())
+                        check_multi_token(tokens, comma)
+                        
+                elif not_equal:
+                        check_legal_token(tokens, not_equal)
+                        decode_token(not_equal.group())
+                        check_multi_token(tokens, not_equal) 
+                        
+                elif explanation_mark:
+                        check_legal_token(tokens, explanation_mark)
+                        decode_token(explanation_mark.group())
+                        check_multi_token(tokens, explanation_mark)  
+                        
+                elif left_bracket:
+                        check_legal_token(tokens, left_bracket)
+                        decode_token(left_bracket.group())
+                        check_multi_token(tokens, left_bracket)  
+                        
+                elif right_bracket:
+                        check_legal_token(tokens, right_bracket)
+                        decode_token(right_bracket.group())
+                        check_multi_token(tokens, right_bracket) 
+                        
+                elif and_percent:
+                        check_legal_token(tokens, and_percent)
+                        decode_token(and_percent.group())
+                        check_multi_token(tokens, and_percent)  
+                        
+                elif left_paren:
+                        check_legal_token(tokens, left_paren)
+                        decode_token(left_paren.group())
+                        check_multi_token(tokens, left_paren)
+                        
+                elif right_paren:
+                        check_legal_token(tokens, right_paren)
+                        decode_token(right_paren.group())
+                        check_multi_token(tokens, right_paren) 
+                        
+                elif plus:
+                        check_legal_token(tokens, plus)
+                        decode_token(plus.group())
+                        check_multi_token(tokens, plus) 
+                        
+                elif minus:
+                        check_legal_token(tokens, minus)
+                        decode_token(minus.group())
+                        check_multi_token(tokens, minus)
+                        
+                elif times:
+                        check_legal_token(tokens, times)
+                        decode_token(times.group())
+                        check_multi_token(tokens, times)   
+                        
+                elif less_than:
+                        check_legal_token(tokens, less_than)
+                        decode_token(less_than.group())
+                        check_multi_token(tokens, less_than)
+                        
+                elif greater_than:
+                        check_legal_token(tokens, greater_than)
+                        decode_token(greater_than.group())
+                        check_multi_token(tokens, greater_than) 
+                        
+                elif less_than_equal:
+                        check_legal_token(tokens, less_than_equal)
+                        decode_token(less_than_equal.group())
+                        check_multi_token(tokens, less_than_equal)  
+                        
+                elif greater_than_equal:
+                        check_legal_token(tokens, greater_than_equal)
+                        decode_token(greater_than_equal.group())
+                        check_multi_token(tokens, greater_than_equal)               
                 else:
                         quit("Error with token " + str(tokens))        
                 
@@ -62,17 +152,91 @@ def decode_token(token):
         equals = equal.match(token) 
         semicolon = semi.match(token)
         cond_or = cond.match(token)
+        comma = comm.match(token)
+        explanation_mark = expl.match(token)
+        left_bracket = left_brack.match(token)
+        right_bracket = right_brack.match(token)
+        and_percent = and_per.match(token)
+        left_paren = left_par.match(token)
+        right_paren = right_par.match(token)
+        plus = add.match(token)
+        minus = sub.match(token)
+        times = mult.match(token)
+        not_equal = not_eq.match(token)
+        less_than = less.match(token)
+        greater_than = greater.match(token)
+        less_than_equal = less_than_eq.match(token)
+        greater_than_equal = greater_than_eq.match(token)       
         
-        LOWERCASE = 1
+        PROGRAM = 1
+        BEGIN = 2
+        END = 3
+        INT = 4
+        IF = 5
+        THEN = 6
+        ELSE = 7
+        WHILE = 8
+        LOOP = 9
+        READ = 10
+        WRITE = 11
         EQUALS = 14
         DOUBLE_EQUALS = 26
         IDENTIFIER = 32
         NUMBER = 31
         SEMICOLON = 12
         OR = 19
+        COMMA = 13
+        EXPLANATION_MARK = 15
+        LEFT_BRACKET = 16
+        RIGHT_BRACKET = 17
+        AND_PERCENT = 18
+        LEFT_PAREN = 20
+        RIGHT_PAREN = 21
+        PLUS = 22
+        MINUS = 23
+        TIMES = 24
+        NOT_EQUAL = 25
+        LESS_THAN = 27
+        GREATER_THAN = 28
+        LESS_THAN_EQUAL = 29
+        GREATER_THAN_EQUAL = 30
         
         if lower:
-                print LOWERCASE
+                if lower.group() == 'program':
+                        print PROGRAM
+                
+                elif lower.group() == 'begin':
+                        print BEGIN
+                
+                elif lower.group() == 'end':
+                        print END
+                
+                elif lower.group() == 'int':
+                        print INT
+                
+                elif lower.group() == 'if':
+                        print IF
+                
+                elif lower.group() == 'then':
+                        print THEN
+                
+                elif lower.group() == 'else':
+                        print ELSE
+                
+                elif lower.group() == 'while':
+                        print WHILE
+                
+                elif lower.group() == 'loop':
+                        print LOOP
+                
+                elif lower.group() == 'read':
+                        print READ
+                
+                elif lower.group() == 'write':
+                        print WRITE
+                
+                else:
+                        quit("Error with token " + str(token)) 
                 
         elif equals:
                 if equals.group() == '=':
@@ -91,6 +255,51 @@ def decode_token(token):
                 
         elif cond_or:
                 print OR
+        
+        elif comma:
+                print COMMA
+                
+        elif not_equal:
+                print NOT_EQUAL
+        
+        elif explanation_mark:
+                print EXPLANATION_MARK
+
+        elif left_bracket:
+                print LEFT_BRACKET
+                
+        elif right_bracket:
+                print RIGHT_BRACKET
+                
+        elif and_percent:
+                print AND_PERCENT
+        
+        elif left_paren:
+                print LEFT_PAREN
+                
+        elif right_paren:
+                print RIGHT_PAREN
+        
+        elif plus:
+                print PLUS
+                
+        elif minus:
+                print MINUS
+        
+        elif times:
+                print TIMES
+                
+        elif less_than:
+                print LESS_THAN
+                
+        elif greater_than:
+                print GREATER_THAN
+        
+        elif less_than_equal:
+                print LESS_THAN_EQUAL
+                
+        elif greater_than_equal:
+                print GREATER_THAN_EQUAL
 
 # Separates tokens that are concatonated together                       
 def check_multi_token(token, list):
@@ -101,15 +310,12 @@ def check_multi_token(token, list):
 
 # Checks if the current token is valid
 def check_legal_token(token, list):
-        special_char = re.compile(';|=|\|') 
-                
+        special_char = re.compile(';|=|\||,|!|\[|\]|&|\(|\)|\+|-|\*|!=|<|>|') 
         if len(token[len(list.group()) :]) != 0 and len(list.group()) !=0:
                 special_token = special_char.match(token[len(list.group())])
                 special_list = special_char.match(list.group())
 
                 if not (special_token or special_list):
-                        #raise Exception("Error with token " + str(token))
-                        #print "Error with token " + str(token)
                         quit("Error with token " + str(token))        
 
 
@@ -135,14 +341,33 @@ if __name__ == "__main__":
         # Regex's
         lowercase = re.compile('[a-z]+')
         identifier = re.compile('[A-Z]+[0-9]*')
-        integers = re.compile('[0-9]+')#[^a-zA-Z]')
+        integers = re.compile('[0-9]+')
         equal = re.compile('==|=')
         semi = re.compile(';')
         cond = re.compile('[|]{2}')
-                
+        comm = re.compile(',')
+        expl = re.compile('!')
+        left_brack = re.compile('\[')
+        right_brack = re.compile('\]')
+        and_per = re.compile('[&]{2}')
+        left_par = re.compile('\(')
+        right_par = re.compile('\)')
+        add = re.compile('\+')
+        sub = re.compile('-')
+        mult = re.compile('\*')
+        not_eq = re.compile('!=')
+        less = re.compile('<')
+        greater = re.compile('>')
+        less_than_eq = re.compile('<=')
+        greater_than_eq = re.compile('>=')
+        EOF = 33
+        
         # Create tokenizer object        
         tokenizer = Tokenizer()
         
         for token in range(len(tokens)):
                 tokenizer.generate_tokens(tokens[token])
+        
+        
+        print EOF
         
